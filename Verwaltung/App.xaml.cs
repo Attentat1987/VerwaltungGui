de65cdbedaee5ef4df7b357b1c8a7765ab4dd20e
Attentat1987 <129548674+Attentat1987@users.noreply.github.com>
2024-05-15 21:52:13 +0200

    Projektdateien hinzufügen.

---
+++ Verwaltung/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Verwaltung
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Verwaltung/Grundlagen/UccGrundlagen.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace Verwaltung.Grundlagen
+{
+    public partial class UccGrundlagen : UserControl
+    {
+        public UccGrundlagen()
+        {
+            InitializeComponent();
+        }
+
+        private void ToggleButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Handler für den Klick auf den ToggleButton
+        }
+    }
+}

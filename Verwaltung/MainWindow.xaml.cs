4cbbcd712d4c3bb011aefee1b1b61424e3d0d133
Attentat1987 <129548674+Attentat1987@users.noreply.github.com>
2024-05-16 14:01:37 +0200

    MinMaxClose events gehen.

--- Verwaltung/MainWindow.xaml.cs
+++ Verwaltung/MainWindow.xaml.cs
@@ -1,14 +1,9 @@
 ﻿using MahApps.Metro.Controls;
-using System.Text;
+using MahApps.Metro.Controls.Dialogs;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Verwaltung
 {
@@ -20,6 +15,69 @@ namespace Verwaltung
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void BtnMinMaxResOnClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button senderBtn)
+            {
+                if (senderBtn == null) return;
+
+                if (senderBtn.Name == nameof(BtnWindowMinimize))
+                {
+                    this.WindowState = WindowState.Minimized;
+                }
+
+                if (senderBtn.Name == nameof(BtnWindowClose))
+                {
+                    var dialogSettings = new MetroDialogSettings
+                    {
+                        AffirmativeButtonText = "JA",
+                        NegativeButtonText = "NEIN",
+                        AnimateShow = true,
+                        AnimateHide = true
+                    };
+
+                    var result = await this.ShowMessageAsync("ACHTUNG", "Wollen Sie wirklich die Anwendung beenden?",
+                        MessageDialogStyle.AffirmativeAndNegative, dialogSettings);
+
+                    if (result == MessageDialogResult.Affirmative)
+                    {
+                        Application.Current.Shutdown();
+                    }
+                }
+            }
+            else if (sender is ToggleButton senderTglBtn)
+            {
+                if (senderTglBtn == null) return;
+
+                if (senderTglBtn.IsChecked == true)
+                {
+                    this.WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Normal;
+                }
+            }
+        }
+
+        private void Grid_PreviewMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            try
+            {
+                if(e.LeftButton == MouseButtonState.Pressed)
+                {
+                    DragMove();
+                }
+
+            }
+            catch (Exception)
+            {
+                //
+            } 
+
+
         }
     }
 }
